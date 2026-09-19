978eab69cad8e000cea4321d9e3715ff11c7ef4d
Gökhan İpek <gipek03@gmail.com>
2026-09-19 17:16:01 +0200

    Updates

---
+++ precache-manifest.0944e737db2d1ade9c0c10a3bfb820b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40eb0d983fffc9a3565328849b0269de",
+    "url": "/pokemon-safari-zone/index.html"
+  },
+  {
+    "revision": "b77db0c53cfcdd2576fc",
+    "url": "/pokemon-safari-zone/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d25c8b8ec2d612768f2f",
+    "url": "/pokemon-safari-zone/static/css/main.daf0989a.chunk.css"
+  },
+  {
+    "revision": "b77db0c53cfcdd2576fc",
+    "url": "/pokemon-safari-zone/static/js/2.4988ada2.chunk.js"
+  },
+  {
+    "revision": "d25c8b8ec2d612768f2f",
+    "url": "/pokemon-safari-zone/static/js/main.3bd32b94.chunk.js"
+  },
+  {
+    "revision": "4446eae4dc2ddf6c8348",
+    "url": "/pokemon-safari-zone/static/js/runtime~main.6ca0041c.js"
+  }
+]);
